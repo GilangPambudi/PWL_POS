9b37dd8182cf59ed158f949d13e1155da5e98a78
GilangPambudi <2131710126@student.polinema.ac.id>
2024-02-29 14:34:02 +0700

    Delete CUS.

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -11,7 +11,9 @@ class LevelController extends Controller
     {   
         // DB::insert('INSERT INTO m_level(level_kode, level_nama, created_at) VALUES (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
 
-        DB::update('UPDATE m_level SET level_nama = ? WHERE level_kode = ?', ['Customer', 'CUS']);
+        // DB::update('UPDATE m_level SET level_nama = ? WHERE level_kode = ?', ['Customer', 'CUS']);
+
+        DB::delete('DELETE FROM m_level WHERE level_kode = ?', ['CUS']);
 
         $row = DB::select('select * from m_level');
         return view('level', ['data' => $row]);
