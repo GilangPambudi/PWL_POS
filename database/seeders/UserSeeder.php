3bb17d702d0e6d6887586f5b2d8c0c2744c046c3
GilangPambudi <2131710126@student.polinema.ac.id>
2024-02-29 15:04:22 +0700

    Fixed 'UserSeeder'.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -43,5 +44,6 @@ class UserSeeder extends Seeder
                 'password' => Hash::make('12345'),
             ],
         ];
+        DB::table('m_user')->insert($data);
     }
 }
