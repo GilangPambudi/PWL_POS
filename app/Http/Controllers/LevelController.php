2ddc87fbec49a2d0899c5fff1f47888a1a5b4e5d
GilangPambudi <2131710126@student.polinema.ac.id>
2024-02-29 14:32:41 +0700

    Test update for CUS at LevelController.

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -9,7 +9,10 @@ class LevelController extends Controller
 {
     public function index()
     {   
-        DB::insert('INSERT INTO m_level(level_kode, level_nama, created_at) VALUES (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        // DB::insert('INSERT INTO m_level(level_kode, level_nama, created_at) VALUES (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+
+        DB::update('UPDATE m_level SET level_nama = ? WHERE level_kode = ?', ['Customer', 'CUS']);
+
         $row = DB::select('select * from m_level');
         return view('level', ['data' => $row]);
     }
