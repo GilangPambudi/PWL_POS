e2dfe8da81eb12ac314c1e1ff23cce19e720f886
GilangPambudi <2131710126@student.polinema.ac.id>
2024-03-07 13:06:47 +0700

    Pertemuan: 4 - Praktikum: 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,16 +13,17 @@ class UserController extends Controller
 
         // tambah data user dengan Eloquent Model
         $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 4
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
 
-        UserModel::insert($data); // tambahkan data
+        UserModel::create($data); // tambahkan data
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_users
         return view('user', ['data' => $user]);
     }
 }
+
