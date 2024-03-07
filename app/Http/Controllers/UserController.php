862436ee945c62e572d98becdfe88939f172fa40
GilangPambudi <2131710126@student.polinema.ac.id>
2024-03-07 14:21:03 +0700

    Pertemuan: 4 - Praktikum: 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,20 +6,32 @@ use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class UserController extends Controller
-{
-    public function index()
-    {
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2,
-            ],
-        );
-        $user ->save();
+class UserController extends Controller{
+    public function index(){
+        $user = UserModel::create([ 
+        'username' => 'manager11', 
+        'nama' => 'Manager11', 
+        'password' => Hash::make('12345'), 
+        'level_id' => 2,
+        ]);
 
-        return view('user', ['data' => $user]);
+        $user->username = 'manager 12';
+
+        $user->isDirty(); // true
+        $user->isDirty('username'); // true
+        $user->isDirty('nama'); // false
+        $user->isDirty(['nama', 'username']); // true
+
+        $user->isClean(); // false
+        $user->isClean('username'); // false
+        $user->isClean('nama'); // true
+        $user->isClean (['nama', 'username']); // false
+
+        $user->save();
+
+        $user->isDirty(); // false
+        $user->isClean(); // true
+
+        dd($user->wasChanged(['nama', 'username']));
     }
 }
